3f5fe9bc02e32de6e08e474581c1f958afb3548c
Cam1101 <jcparradoc@javeriana.edu.co>
2024-05-27 20:21:14 -0500

    Fix: se arregla el puerto

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from recommender import recommend_categories
 
@@ -17,4 +18,5 @@ def recommend():
     return jsonify({"recommended_categories": recommended_categories})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
